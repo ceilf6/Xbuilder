27d4558ac656b388074416e724f70ce9ee91e4ab
Long <long@Long-Mac.local>
2025-08-14 15:00:11 +0800

    modify qqshare test code from projectvue to projectsharemodal

--- spx-gui/src/components/project/ProjectShareModal.vue
+++ spx-gui/src/components/project/ProjectShareModal.vue
@@ -149,6 +149,15 @@
     resolved: []
   }>()
 
+  const shareData = {
+        title:    'QQ分享卡片示例',
+        desc:     '演示QQ卡片分享功能',
+        share_url:'https://xbuilder-sharing-test.gopluscdn.com/',
+        image_url:'https://i.gtimg.cn/open/app_icon/05/58/35/77/1105583577_100_m.png'
+    };
+
+    window.mqq.invoke('data', 'setShareInfo', shareData);
+
   const selectedPlatform = ref('qq')
 
   // const showQRCode = ref(false) // 是否显示二维码
