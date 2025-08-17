df7b98600b449573f83332cfa299919a69cd8376
wjh <3506456886@qq.com>
2025-08-17 12:23:37 +0800

    fix: 修复在win上误更改ts解析

--- spx-gui/src/components/editor/code-editor/lsp/spxls/client.ts
+++ spx-gui/src/components/editor/code-editor/lsp/spxls/client.ts
@@ -1,5 +1,1 @@
-// 错误内容为路径引用，应该为模块导入或软链接。临时注释或删除此行，避免 Vite 构建报错。
-// 原内容: ../../../../../../../tools/spxls/client.ts
-
-// 重新导出 tools/spxls/client.ts 的所有内容，解决 ResponseError 导出问题
-export * from '../../../../../../../tools/spxls/client.ts';
+../../../../../../../tools/spxls/client.ts
