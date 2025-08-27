e8e484d07062401e2cd85d644a7dc1bbf2362f5e
Lotterng <759932827@qq.com>
2025-08-27 20:41:05 +0800

    refactor:修改直接分享逻辑

--- docs/develop/share/module_ProjectDirectSharing.ts
+++ docs/develop/share/module_ProjectDirectSharing.ts
@@ -2,54 +2,56 @@
  * 直接分享组件
 */
 // 导入必要的类型和函数
-import { PlatformConfig } from "./platformShare"
-import Poster from "./poster"
-import { defineProps, ref } from "vue"
+import { PlatformConfig, SocialPlatformConfigs} from "./module_platformShare"
+import { getProjectPageRoute } from "@/router"
+import { createPoster } from "./module_poster"
+import { defineProps, defineEmits, ref} from "vue"
 /**
  * 定义组件的props
  */
 const props = defineProps<{
     projectData: {
         // 项目数据
-        // 项目URL
-        url: string
+        // 项目唯一标识
+        owner: string
+        name: string
         // 项目缩略图
         thumbnail: string
-    },
-    platform: PlatformConfig
+    }
 }>()
 
 // 以下是伪代码
 /**
  * 模拟qrcode返回的DataURL
  */
+// 默认选择第一个平台
+const selectPlatform =  ref<PlatformConfig>(SocialPlatformConfigs[0])
+
 const qrcode = {
     toDataURL(url: string) {
         return `data:image/png;base64,${url}`
     }
 }
-// 默认选择第一个平台
-const selectPlatform = props.platform
 // 二维码地址 - 默认空字符串
-let DataURL: string = ''
+const DataURL = ref<string>('')
 
 
 // 模拟poster返回的图片
-const posterCompRef = ref<InstanceType<Poster>>()
-const posterData = await posterCompRef.value.createPoster({ img: props.screenShot, ProjectData: props.projectData})
+const posterData = await createPoster()
 
 // 模拟平台切换
 async function handPlatformChange(platform: PlatformConfig) {
+    
+    selectPlatform.value =  platform
+    
     if (platform.shareType.supportProject && platform.shareFunction.shareURL) {
-        DataURL = qrcode.toDataURL(await platform.shareFunction.shareURL(props.projectData.url))
+        DataURL.value = qrcode.toDataURL(await platform.shareFunction.shareURL(getProjectPageRoute(props.projectData.owner, props.projectData.name)))
     }
     else if (platform.shareType.supportPoster && platform.shareFunction.shareImage) {
-        DataURL = qrcode.toDataURL(await platform.shareFunction.shareImage(posterData))
+        DataURL.value = qrcode.toDataURL(await platform.shareFunction.shareImage(posterData))
     }
     else {
-        DataURL = ''
+        DataURL.value = ''
     }
 }
-// 初始化时运行一次
-handPlatformChange(selectPlatform)
 
